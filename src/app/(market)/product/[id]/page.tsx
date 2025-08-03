1083892f17a0e3f8712a153a447542daeee4c3c7
mkstas <makarov.stn@gmail.com>
2025-08-04 01:01:23 +0300

    feat: add functional on single product page

--- src/app/(market)/product/[id]/page.tsx
+++ src/app/(market)/product/[id]/page.tsx
@@ -1,14 +1,25 @@
 import { prisma } from '@/prisma/prisma-client';
 import { Container } from '@/components/shared/container';
 import { notFound } from 'next/navigation';
-import { ProductImage } from '@/components/shared/product-image';
-import { Title } from '@/components/shared/title';
-import { GroupVariants } from '@/components/shared/group-variants';
+import { ProductForm } from '@/components/shared/product-form';
 
 export default async function Poduct({ params: { id } }: { params: { id: string } }) {
   const product = await prisma.product.findFirst({
     where: {
       id: Number(id),
+    },
+    include: {
+      productVariants: true,
+      ingredients: true,
+      category: {
+        include: {
+          products: {
+            include: {
+              productVariants: true,
+            },
+          },
+        },
+      },
     },
   });
 
@@ -18,30 +29,7 @@ export default async function Poduct({ params: { id } }: { params: { id: string 
 
   return (
     <Container className="my-10 flex flex-col">
-      <div className="flex flex-1">
-        <ProductImage imageUrl={product.imageUrl} size={40} />
-        <div className="bg-secondary w-[490px] rounded-md p-7">
-          <Title text={product.name} size="md" className="mb-1 font-extrabold" />
-          <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur.</p>
-          <GroupVariants
-            value="2"
-            items={[
-              {
-                name: 'Маленькая',
-                value: '1',
-              },
-              {
-                name: 'Средняя',
-                value: '2',
-              },
-              {
-                name: 'Большая',
-                value: '3',
-              },
-            ]}
-          />
-        </div>
-      </div>
+      <ProductForm product={product} />
     </Container>
   );
 }
--- src/components/shared/modal/choose-product-modal.tsx
+++ src/components/shared/modal/choose-product-modal.tsx
@@ -4,12 +4,9 @@ import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import { FC } from 'react';
-import { ChoozeProductForm } from '../chooze-product-form';
 import { ProductWithRelations } from '@/@types/product';
-import { ChoozePizzaForm } from '../chooze-pizza-form';
 import { DialogTitle } from '@radix-ui/react-dialog';
-import { useCartStore } from '@/store/cart';
-import toast from 'react-hot-toast';
+import { ProductForm } from '../product-form';
 
 interface Props {
   product: ProductWithRelations;
@@ -18,60 +15,12 @@ interface Props {
 
 export const ChooseProductModal: FC<Props> = ({ className, product }) => {
   const router = useRouter();
-  const firstItem = product.productVariants[0];
-  const isPizzaForm = Boolean(firstItem.type);
-
-  const { loading, addCartItem } = useCartStore();
-
-  const onAddToCart = async () => {
-    try {
-      await addCartItem({
-        productVariantId: firstItem.id,
-      });
-      toast.success('Товар добавлен в корзину');
-      router.back();
-    } catch (error) {
-      toast.error('Не удалось добавить товар в корзину');
-      console.error(error);
-    }
-  };
-
-  const onAddPizzaToCart = async (productVariantId: number, ingredients: number[]) => {
-    try {
-      await addCartItem({
-        productVariantId,
-        ingredients,
-      });
-      toast.success('Товар добавлен в корзину');
-      router.back();
-    } catch (error) {
-      toast.error('Не удалось добавить товар в корзину');
-      console.error(error);
-    }
-  };
 
   return (
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
       <DialogTitle hidden>{product.name}</DialogTitle>
       <DialogContent className={cn('min-h-[520px] overflow-hidden bg-white p-0 sm:max-w-5xl', className)}>
-        {isPizzaForm ? (
-          <ChoozePizzaForm
-            imageUrl={product.imageUrl}
-            name={product.name}
-            ingredients={product.ingredients}
-            items={product.productVariants}
-            onSubmit={onAddPizzaToCart}
-            loading={loading}
-          />
-        ) : (
-          <ChoozeProductForm
-            imageUrl={product.imageUrl}
-            name={product.name}
-            price={firstItem.price}
-            onSubmit={onAddToCart}
-            loading={loading}
-          />
-        )}
+        <ProductForm product={product} />
       </DialogContent>
     </Dialog>
   );
