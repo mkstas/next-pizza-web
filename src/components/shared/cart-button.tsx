dbdc266b17be60b7690bc81cc609d17d0f0cbdb3
mkstas <makarov.stn@gmail.com>
2025-08-03 04:20:12 +0300

    feat: display info in cart button

--- src/components/shared/cart-button.tsx
+++ src/components/shared/cart-button.tsx
@@ -1,22 +1,27 @@
+'use client';
+
 import { ShoppingCart, ArrowRight } from 'lucide-react';
 import { Button } from '../ui/button';
 import { FC } from 'react';
 import { cn } from '@/lib/utils';
 import { CartDrawer } from './cart-drawer';
+import { useCartStore } from '@/store/cart';
 
 interface Props {
   className?: string;
 }
 
 export const CartButton: FC<Props> = ({ className }) => {
+  const { cost, loading, cartProduct } = useCartStore();
+
   return (
     <CartDrawer>
-      <Button className={cn('group relative', className)} size={'lg'}>
-        <span className="font-bold">520 ₽</span>
+      <Button loading={loading} className={cn('group relative', className)} size={'lg'}>
+        <span className="font-bold">{cost} ₽</span>
         <span className="mx-3 h-6 w-0.25 bg-white/30" />
         <span className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
           <ShoppingCart size={16} className="relative mr-1" strokeWidth={2} />
-          <span className="font-bold">3</span>
+          <span className="font-bold">{cartProduct.length}</span>
         </span>
         <ArrowRight
           size={20}
