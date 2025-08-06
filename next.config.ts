f5dfcbccb2b60acb663e6ff845f3060b6e15acb0
mkstas <makarov.stn@gmail.com>
2025-08-06 03:38:20 +0300

    feat: install additional dependencies

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://media.dodostatic.net/**'), new URL('https://cdn.dodostatic.net/**')],
   },
+  devIndicators: false,
 };
 
 export default nextConfig;
